6a1b10216f159cdff8d20fba7281a088ab78da0b
CharlesLi <lgc850106@qq.com>
2022-05-29 23:38:11 +0000

    fit the . replit file add /name URL in mydemoapp. py

--- mydemoapp.py
+++ mydemoapp.py
@@ -5,6 +5,10 @@ app = Flask(__name__)
 @app.route("/")
 def hello():
     return "Hello! This is a basic Flask app. "
+  
+@app.route("/<name>")
+def hello_name(name):
+    return f"Hello {name}"
 
 if __name__ == "__main__":
     app.run("0.0.0.0")
